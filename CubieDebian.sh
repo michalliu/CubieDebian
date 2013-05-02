4001191c265f43c52814371629521eb6d0ed3aca
michalliu <michalliu@foxmail.com>
2013-05-02 18:05:43 +0800

    the default screen resolution is 1280*800 instead of 1280*20

--- CubieDebian.sh
+++ CubieDebian.sh
@@ -139,7 +139,7 @@ END
 
 configUBoot() {
 cat > ./${DEB_HOSTNAME}-armfs/boot/boot.cmd <<END
-setenv bootargs console=tty0 console=ttyS0,115200 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x20p60 root=/dev/mmcblk0p1 rootwait panic=10 ${extra}
+setenv bootargs console=tty0 console=ttyS0,115200 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x800p60 root=/dev/mmcblk0p1 rootwait panic=10 ${extra}
 ext2load mmc 0 0x43000000 boot/script.bin
 ext2load mmc 0 0x48000000 boot/uImage
 bootm 0x48000000
