851f7e4e1dd1895ebfa2a8ba1150d23d0fa97da6
michalliu <michalliu@foxmail.com>
2013-05-29 11:17:40 +0800

    Update buildApache.sh

--- buildApache.sh
+++ buildApache.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "http://stackoverflow.com/questions/8188158/building-and-configuring-apr-util-and-httpd-to-use-apr-iconv-on-linux"
+echo "http://httpd.apache.org/docs/2.2/install.html"
 promptyn () {
 while true; do
   read -p "$1 " yn
