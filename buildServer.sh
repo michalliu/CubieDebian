7098d81766988326c6637d89e47cb5909e6a438a
michalliu <michalliu@foxmail.com>
2013-05-31 11:36:19 +0800

    Update buildServer.sh
    add haproxy to buildlist

--- buildServer.sh
+++ buildServer.sh
@@ -42,7 +42,7 @@ done
 
 IMGFILE=${IMAGEFILE_OPT:-./Cubian-server-r1-arm.img}
 DESTDIR=${DESTDIR_OPT:-./mnt}
-TARGETS=("apache" "mysql" "redis" "php" "nginx" "lighthttpd" "varnish" "memcached" "mongodb" "jre" "node")
+TARGETS=("apache" "mysql" "redis" "php" "nginx" "lighthttpd" "varnish" "haproxy" "memcached" "mongodb" "jre" "node")
 
 if [ ! -z "${HELP_OPT:-}" ];then
     usage
