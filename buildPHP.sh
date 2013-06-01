ba667eba1839b8954723de7457522d4a9967fd92
michalliu <michalliu@foxmail.com>
2013-06-02 05:41:49 +0800

    update php builder

--- buildPHP.sh
+++ buildPHP.sh
@@ -8,59 +8,120 @@ CONFIGURE="./configure"
 
 PHP5_DIR="${CWD}/php5"
 PHP5_CONFIGURATION=" \
---prefix=/opt/php/5.4.15 \
---with-zlib-dir \
---with-freetype-dir \
---enable-cgi \
---enable-mbstring \
---with-libxml-dir=/usr \
---enable-soap \
---enable-calendar \
---with-curl \
---with-mcrypt \
---with-zlib \
---with-gd \
+--prefix=/usr/share/php52 \
+--datadir=/usr/share/php52 \
+--mandir=/usr/share/man \
+--bindir=/usr/bin/php52 \
+--with-libdir=lib64 \
+--includedir=/usr/include \
+--sysconfdir=/etc/php52/apache2 \
+--with-config-file-path=/etc/php52/cli \
+--with-config-file-scan-dir=/etc/php52/conf.d \
+--localstatedir=/var \
+--disable-debug \
+--with-regex=php \
 --disable-rpath \
---enable-inline-optimization \
---with-bz2 \
---with-zlib \
---enable-sockets \
+--disable-static \
+--disable-posix \
+--with-pic \
+--with-layout=GNU \
+--with-pear=/usr/share/php \
+--enable-calendar \
 --enable-sysvsem \
 --enable-sysvshm \
---enable-pcntl \
---enable-mbregex \
---with-mhash \
---enable-zip \
---with-pcre-regex \
---with-mysql \
---with-pdo-mysql \
---with-mysqli \
---with-jpeg-dir=/usr \
---with-png-dir=/usr \
---enable-gd-native-ttf \
---with-openssl \
---with-libdir=lib64 \
---with-libxml-dir=/usr \
---enable-exif \
---enable-dba \
---with-gettext \
---enable-shmop \
 --enable-sysvmsg \
---enable-wddx \
---with-imap \
---with-imap-ssl \
---with-kerberos \
 --enable-bcmath \
+--with-bz2 \
+--enable-ctype \
+--with-db4 \
+--without-gdbm \
+--with-iconv \
+--enable-exif \
 --enable-ftp \
---enable-intl \
---with-pspell"
+--enable-cli \
+--with-gettext \
+--enable-mbstring \
+--with-pcre-regex=/usr \
+--enable-shmop \
+--enable-sockets \
+--enable-wddx \
+--with-libxml-dir=/usr \
+--with-zlib \
+--with-kerberos=/usr \
+--with-openssl=/usr \
+--enable-soap \
+--enable-zip \
+--with-mhash \
+--with-exec-dir=/usr/lib/php5/libexec \
+--without-mm \
+--with-curl=shared,/usr \
+--with-zlib-dir=/usr \
+--with-gd=shared,/usr \
+--enable-gd-native-ttf \
+--with-gmp=shared,/usr \
+--with-jpeg-dir=shared,/usr \
+--with-xpm-dir=shared,/usr/X11R6 \
+--with-png-dir=shared,/usr \
+--with-freetype-dir=shared,/usr \
+--with-ttf=shared,/usr \
+--with-t1lib=shared,/usr \
+--with-ldap=shared,/usr \
+--with-mysql=shared,/usr \
+--with-mysqli=shared,/usr/bin/mysql_config \
+--with-pgsql=shared,/usr \
+--with-pspell=shared,/usr \
+--with-unixODBC=shared,/usr \
+--with-xsl=shared,/usr \
+--with-snmp=shared,/usr \
+--with-sqlite=shared,/usr \
+--with-tidy=shared,/usr \
+--with-xmlrpc=shared \
+--enable-pdo=shared \
+--without-pdo-dblib \
+--with-pdo-mysql=shared,/usr \
+--with-pdo-pgsql=shared,/usr \
+--with-pdo-odbc=shared,unixODBC,/usr \
+--with-pdo-dblib=shared,/usr \
+--enable-force-cgi-redirect  --enable-fastcgi \
+--with-libdir=/lib/arm-linux-gnueabihf \
+--with-pdo-sqlite=shared \
+--with-sqlite=shared \
+--enable-ipv6 \
+--with-mcrypt \
+--with-imap-ssl"
 
 if promptyn "process php5?";then
     cd $PHP5_DIR
-    installpackages "libxml2-dev" "re2c" "bison" "libssl-dev"
+    installpackages "libxml2-dev" \
+"re2c" \
+"bison" \
+"libssl-dev" \
+"libpcre3-dev" \
+"libbz2-dev" \
+"libcurl4-openssl-dev" \
+"libdb5.1-dev" \
+"libjpeg8-dev" \
+"libpng12-dev" \
+"libxpm-dev" \
+"libfreetype6-dev" \
+"libmysqlclient-dev" \
+"postgresql-server-dev-9.1" \
+"libt1-dev" \
+"libgd2-xpm-dev" \
+"libgmp-dev" \
+"libsasl2-dev" \
+"libmhash-dev" \
+"unixodbc-dev" \
+"freetds-dev" \
+"libpspell-dev" \
+"libsnmp-dev" \
+"libtidy-dev" \
+"libxslt1-dev" \
+"libmcrypt-dev"
+
     if promptyn "configure php5?";then
         echo "configure php5 with configuration $PHP5_CONFIGURATION"
-        $CONFIGURE -q $PHP5_CONFIGURATION
+        $CONFIGURE $PHP5_CONFIGURATION
     fi
     if promptyn "make php5?";then
         make -C $PHP5_DIR
