c4317f96636499be9cfb774ada28408e20bc97de
michalliu <michalliu@foxmail.com>
2013-06-06 11:28:23 +0800

    Update CubieDebian.sh
    verbose debootstrap

--- CubieDebian.sh
+++ CubieDebian.sh
@@ -125,8 +125,7 @@ if [ -d ${ROOTFS_DIR} ];then
     rm -rf ${ROOTFS_DIR}
 fi
 mkdir --parents ${ROOTFS_DIR}
-#debootstrap --foreign --arch armhf wheezy ${ROOTFS_DIR}/ http://mirrors.sohu.com/debian/
-debootstrap --foreign --arch armhf wheezy ${ROOTFS_DIR}/ http://http.debian.net/debian/
+debootstrap --foreign --verbose --arch armhf wheezy ${ROOTFS_DIR}/ http://http.debian.net/debian/
 }
 
 installBaseSys(){
