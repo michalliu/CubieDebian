6eb194ed40d0ab7a9767f70e9325de42608d78ee
michalliu <michalliu@foxmail.com>
2013-07-14 15:41:19 +0800

    delete some file

--- scripts/etc/morseshowip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ipaddr=`/etc/getip.sh`
-lockfile='/var/lock/morsemsg'
-if [ "$ipaddr" != '' ];then
-    if [ -f $lockfile ];then
-        rm -f $lockfile
-    fi
-    /etc/morsemsg.sh $ipaddr
-fi
